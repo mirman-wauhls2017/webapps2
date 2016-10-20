5f85bb43cd1dc8c4815da03807e96eccc26d2968
Walker Uhls <wauhls2017@mirmanmustangs.org>
2016-10-20 14:36:31 -0700

    Finished enter the node primary objective

--- 3-enter-the-node/hello.js
+++ 3-enter-the-node/hello.js
@@ -1,1 +1,10 @@
-console.log("Hello from Node!");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log("it's working m8");
+});
