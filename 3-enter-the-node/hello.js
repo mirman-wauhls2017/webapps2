772ae14c3f17125271a0582e2e667f588f9a8a53
mirman-wuhls2017 <wauhls2017@mirmanmustangs.org>
2016-10-31 14:47:07 -0700

    Truly finished 3

--- 3-enter-the-node/hello.js
+++ 3-enter-the-node/hello.js
@@ -9,6 +9,7 @@ app.get("/greet/:name", function(req,res)) {
   var name = req.params.name;
   res.send("<hi>Hi, $(name)!")
 }
+app.use('/static', express.static('public'));
 
 app.listen(3000, function () {
   console.log("it's working m8");
