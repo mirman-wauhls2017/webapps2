22812959fbffe73051845e695c72dbd54ec18eab
Walker <wauhls2017@mirmanmustangs.org>
2016-12-12 14:38:50 -0800

    finished get, working on delete

--- 5-load-from-node/index.js
+++ 5-load-from-node/index.js
@@ -1,51 +1,71 @@
+'use strict';
+
 const express = require("express");
+const bodyParser = require("body-parser")
 const fs = require("fs");
 const port = 3000;
 
 const app = express();
 
-function loadSingleTodo(res, id) {
-  return fs.readFile("./todos.json", (err, raw) => {
-      if (err) throw err;
-      todos = JSON.parse(raw).data;
-      for(const t in todos){
-        const todo = todos[t];
-        console.log(todo.text);
-        if (todo.completed == true) console.log("You have completed this todo.");
-        else console.log("You haven't finished this todo.");
+app.use(bodyParser.json());
 
-
-      }
-  });
-}
-
-function loadTodos(res) {
+function loadTodos(callback) {
   return fs.readFile("./todos.json", (err, data) => {
     if (err) throw err;
-    res.json(JSON.parse(data));
+    callback(JSON.parse(data));
   });
+}
+function isKeptId (kId,rId) {
+  return kId != rId;
 }
 
 app.route("/todos")
 .get((req,res) => {
-  loadTodos(res);
+  loadTodos((json) => {
+    res.json(json.data);
+  });
 })
 .post((req,res) => {
-  res.send("Creating a todo");
+  let newTodo = req.body;
+  newTodo.completed = false;
+  console.log(newTodo);
+  loadTodos((json) => {
+    json.data.push(newTodo);
+    json.lastId++;
+    newTodo.id = json.lastId;
+    fs.writeFile("./todos.json", JSON.stringify(json), (err) => {
+      if (err) throw err;
+      res.status(200).end();
+    });
+  });
 });
 
 app.route("/todos/:id")
 .get((req,res) => {
-  const id = req.params.id;
-  loadSingleTodo(res, id);
+  const id = parseInt(req.params.id);
+  loadTodos((json) => {
+    const todos = json.data;
+    for (const todo of todos) {
+      if (todo.id === id) {
+      return res.json(todo);
+      }
+    }
+  })
 })
 .put((req,res) => {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
   res.send(`Updating todo #${id}`)
 })
 .delete((req,res) => {
-  const id = req.params.id;
-  res.send(`Deleting todo #${id}`);
+  const id = parseInt(req.params.id);
+  loadTodos((json) => {
+    const todos = json.data;
+    const allIds = json.data.map((i) => {return i.id;});
+    console.log(allIds);
+    var result = allIds.filter(isKeptId);
+    console.log(result);
+    res.send(`Deleting todo #${id}`);
+  });
 });
 
 app.listen(port, () => {
