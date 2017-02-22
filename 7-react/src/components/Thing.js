6d07478b5ea53ba98b420bb09b3411e9f3a82e51
mirman-wauhls2017 <wauhls2017@mirmanmustangs.org>
2017-02-22 14:07:30 -0800

    Finished 7

---
+++ 7-react/src/components/Thing.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Thing extends React.Component {
+
+  render() {
+    return (
+      <div className="good">
+        <ul>
+          <li><h2>Hey, that's pretty {this.props.good}</h2></li>
+          <li><h5>Hey, that's pretty {this.props.good}</h5></li>
+          <li><p>Hey, that's pretty {this.props.good}</p></li>
+          <li><ul><li><h2>Hey, that's pretty {this.props.good}</h2></li>
+          <li><h5>Hey, that's pretty {this.props.good}</h5></li>
+          <li><p>Hey, that's pretty {this.props.good}</p></li></ul></li>
+        </ul>
+      </div>
+    );
+  }
+}
--- 7-react/src/index.js
+++ 7-react/src/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import { render } from "react-dom";
 import Message from "./components/Message";
+import Thing from "./components/Thing";
 
 class App extends React.Component {
 
   render() {
-    const message = "I'm a child component!";
+    const messages = [
+      "Hey, that's pretty good",
+      "Hey, that's pretty bad",
+      "Hey, that's totally pretty solid"
+    ];
+    const goods = [
+      "good",
+      "bad",
+      "solid",
+      "webpack",
+      "combo",
+      "frost",
+      "krusher99",
+      "securly",
+      "dumb"
+    ]
     return (
       <div>
         <h1>Hello from React!</h1>
-        <Message message={message} />
+        {
+          messages.map((m) => {
+            return (
+              <Message message={m} />
+            )
+          })
+        }
+        {
+          goods.map((m) => {
+            return (
+              <Thing good={m} />
+            )
+          })
+        }
       </div>
     )
   }
